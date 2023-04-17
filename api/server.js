0f7aba6483b1194b1ace2b3b25d2345c1d3158fb
oguzhanalper06 <alperhanoguz06@gmail.com>
2023-04-17 22:53:12 +0300

    server yapılandırıldı

--- api/server.js
+++ api/server.js
@@ -1,5 +1,22 @@
-const express = require('express');
+const express = require("express");
 const server = express();
+
+server.use(express.json());
+
+require("dotenv").config();
+
+const actionsRouter = require("./actions/actions-router");
+server.use("./api/actions", actionsRouter);
+
+const projectsRouter = require("./projects/projects-router");
+server.use("./api/projects", projectsRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({
+    statusCode: 200,
+    message: process.env.MESSAGE || "Server is up and runing..",
+  });
+});
 
 // Sunucunuzu yapılandırın
 // Eylem routerınızı /api/actions/actions-router.js içinde oluşturun
